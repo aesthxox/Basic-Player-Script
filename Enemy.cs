b9d51267b4e3cb8e335436297968bd8e29007c45
aesthxox <79872827+aesthxox@users.noreply.github.com>
2023-04-19 20:50:35 -0700

    Add files via upload
    This is the Enemy Script

---
+++ Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject player;
+    public int speed = 4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(player.transform);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        
+    }
+}
